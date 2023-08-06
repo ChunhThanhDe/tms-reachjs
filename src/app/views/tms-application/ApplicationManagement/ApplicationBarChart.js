ab17c0303775867a4e600890af4a367c861b00ad
Nguyen Thanh Chung 1 <chungnt1@vnpt-technology.vn>
2023-08-06 17:00:21 +0700

    map với main

---
+++ src/app/views/tms-application/ApplicationManagement/ApplicationBarChart.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import BarChart from 'app/components/Chart/BarChart';
+import { getApplicationBarChart } from 'app/Services/StudioServices';
+import StackBarChart from 'app/components/Chart/StackBarChart';
+
+const ApplicationBarChart = () => {
+  const [arr, setArr] = useState([]);
+
+  const handleGetData = async () => {
+    let response = await getApplicationBarChart();
+    console.log(response);
+    if (response.status === 200) {
+      setArr(response.data);
+    }
+  };
+  useEffect(() => {
+    handleGetData();
+    const intervalId = setInterval(() => {
+      handleGetData();
+    }, 180000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <StackBarChart data={arr} legend={['Unconnected Devices', 'Connecting Devices']} />;
+};
+
+export default ApplicationBarChart;
